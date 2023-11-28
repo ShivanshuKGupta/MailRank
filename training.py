10b33211883a9aba3f30d2220467ffdd83f6cd6c
Shivanshu Gupta <shivanshukgupta@gmail.com>
2023-11-29 01:57:43 +0530

    Some Update

--- training.py
+++ training.py
@@ -1,15 +1,40 @@
 # %%
-from nltk.stem import PorterStemmer
+import pandas as pd
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer, WordNetLemmatizer
+from nltk.corpus import stopwords
+import nltk
 import pandas as pd
 
-df = pd.read_csv('preprocessed_text_data.csv')
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+
+# %%
+df = pd.read_csv('preprocessed_data.csv')
 
 
-def apply_stemming(txt):
-    words = txt.split()
+def preprocess_text(text):
+    # tokens = word_tokenize(text)
+    # stop_words = set(stopwords.words('english'))
+    # tokens = [word for word in tokens if word.lower() not in stop_words]
+
+    print(f"{text=}")
+    tokens = text
+    print(f"{tokens=}")
+    stemmer = PorterStemmer()
+    stemmed_words = [stemmer.stem(word) for word in tokens]
+
+    lemmatizer = WordNetLemmatizer()
+    lemmatized_words = [lemmatizer.lemmatize(word) for word in stemmed_words]
+
+    preprocessed_text = ' '.join(lemmatized_words)
+    return preprocessed_text
 
 
-stemmer = PorterStemmer()
-stemmed_words = [stemmer.stem(word) for word in words]
+columns_to_preprocess = df.columns.drop('target')
 
-stemmed_words
+for column in columns_to_preprocess:
+    df[column] = df[column].apply(preprocess_text)
+
+# df
