db0e3ddfe37901e9c72e90df4ef63df09390b0bb
Shivanshu Gupta <shivanshukgupta@gmail.com>
2023-11-29 01:18:12 +0530

    Another Update

--- preprocessing.py
+++ preprocessing.py
@@ -21,23 +21,24 @@ print(df.columns)
 print("\nOriginal Data:")
 print(df.head())
 
-# Ensure that 'target' is present in the dataframe
-if 'target' in df.columns:
+# Ensure that 'content' is present in the dataframe
+if 'content' in df.columns:
     # Text Cleaning: Remove special characters, numbers, and convert to lowercase
     def clean_text(text):
         # Check for NaN values
         if pd.isnull(text):
             return ""
-        
+
         text = str(text)  # Convert to string if it's not already
-        text = re.sub(r'[^a-zA-Z\s]', '', text, re.I | re.A)  # Remove non-alphabetic characters
+        # Remove non-alphabetic characters
+        text = re.sub(r'[^a-zA-Z\s]', '', text, re.I | re.A)
         text = text.lower()  # Convert to lowercase
         return text
 
-    df['target'] = df['target'].apply(clean_text)
+    df['content'] = df['content'].apply(clean_text)
 
     # Tokenization: Break text into words
-    df['target'] = df['target'].apply(word_tokenize)
+    df['content'] = df['content'].apply(word_tokenize)
 
     # Handling Stop Words: Remove common words that do not contribute much to the meaning
     stop_words = set(stopwords.words('english'))
@@ -46,10 +47,10 @@ if 'target' in df.columns:
         return [word for word in tokens if word.lower() not in stop_words]
 
     # Join the lists of tokens back into strings
-    df['target'] = df['target'].apply(lambda x: ' '.join(x))
+    df['content'] = df['content'].apply(lambda x: ' '.join(x))
 
     # Apply stop words removal
-    df['target'] = df['target'].apply(remove_stopwords)
+    df['content'] = df['content'].apply(remove_stopwords)
 
     # Save the preprocessed data to a new CSV file
     df.to_csv('preprocessed_text_data.csv', index=False)
@@ -58,7 +59,7 @@ if 'target' in df.columns:
     print("\nPreprocessed Text Data:")
     print(df)
 else:
-    print("The 'target' column does not exist in the dataframe.")
+    print("The 'content' column does not exist in the dataframe.")
 
 
 # %%
