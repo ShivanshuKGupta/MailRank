fcb697eb6e5fbbd30c36420108bf8c3995112f7b
Shivanshu Gupta <shivanshukgupta@gmail.com>
2023-11-28 23:27:09 +0530

    Update 2

--- scrape_data.py
+++ scrape_data.py
@@ -6,10 +6,11 @@ m_Options = webdriver.ChromeOptions()
 # m_Options.add_argument("--headless")
 # m_Options.add_argument("--no-sandbox")
 m_Options.add_argument(
-    "--user-data-dir=C:\\Users\\praya\\AppData\\Local\\Google\\Chrome\\User Data")
+    "--user-data-dir=C:\\Users\\Sonu\\AppData\\Local\\Google\\Chrome\\User Data")
 m_Options.add_argument("--profile-directory=Profile 1")
 m_Options.add_argument("--disable-extensions")
-driver = webdriver.Chrome('C:\\Users\\praya\\Downloads\\chromedriver_win32\\chromedriver.exe', options=m_Options)
+# 'C:\\Users\\Sonu\\Downloads\\chromedriver_win32\\chromedriver.exe'
+driver = webdriver.Chrome(options=m_Options)
 
 driver.get('http://mail.google.com')
 first_mail = driver.find_element(by="class name", value="bog")
@@ -17,9 +18,15 @@ first_mail = driver.find_element(by="class name", value="bog")
 print("opening first mail...")
 first_mail.click()
 
-pd.write_csv("data.csv", ['heading', 'sender_name', 'sender_email', 'content'])
+data = {
+    'heading': [],
+    'sender_name': [],
+    'sender_email': [],
+    'content': [],
+}
 
-while (True):
+mails_done = 0
+while (mails_done < 100):
     try:
         heading = driver.find_element(by="class name", value="hP").text
         sender_name = driver.find_element(by="class name", value="gD").text
@@ -38,18 +45,22 @@ while (True):
             continue
 
         print(f"{heading=}")
+        data['heading'].append(heading)
         print(f"{sender_name=}")
+        data['sender_name'].append(sender_name)
         print(f"{sender_email=}")
+        data['sender_email'].append(sender_email)
         print(f"{div_element=}")
         content.replace('\n', ' ')
         content.replace('\t', ' ')
         content.replace('\r', ' ')
         print(f"{content=}")
+        data['content'].append(content)
         print(f"{next_button=}")
         print("Clicking next button")
-        pd.write_csv("data.csv", [heading, sender_name, sender_email, content], mode='a')
         # input("Press enter to move to next email")
         next_button.click()
+        mails_done += 1
     except:
         print(f"Next button was not found. Retrying...", end='')
         time.sleep(0.5)
@@ -57,6 +68,8 @@ while (True):
         continue
     retry = 100
 
+df = pd.DataFrame(data)
+df.to_csv("data.csv", index=False)
 print("Done")
 input("Press Enter to Quit")
 driver.quit()
