71aaab69420d5e340f55185da31eb6bf0f831ece
John Doe <cs21b1022@iiitr.ac.in>
2023-11-29 10:50:23 +0530

    hehe

--- feature_extraction.py
+++ feature_extraction.py
@@ -5,7 +5,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 #reading the data
-data = pd.read_csv('preprocessed_text_data.csv')
+data = pd.read_csv('preprocessed_data.csv')
 
 #creating a bag of words model
 cv = CountVectorizer()
